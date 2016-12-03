b7e32bff238dfd16066226c8805fe67edb8e6541
OliverZShen96 <oliver.z.shen96@gmail.com>
2016-12-03 18:38:04 +1100

    Completed day 2

--- src/Day2.java
+++ src/Day2.java
@@ -4,59 +4,80 @@ import java.io.IOException;
 
 public class Day2 {
 	
-	public static int x = 1;
-	public static int y = 1;
+	// starting places
+	public static int x1 = 1;
+	public static int y1 = 1;
+	
+	public static int x2 = 0;
+	public static int y2 = 2;
+	
+	// keypad data for part 1
+	public static final char[][] keypadNums = {{'1','4','7'},{'2','5','8'},{'3','6','9'}};
+	
+	// keypad data for part 2
+	public static final char[] col1 = {'x','x','5','x','x'};
+	public static final char[] col2 = {'x','2','6','A','x'};
+	public static final char[] col3 = {'1','3','7','B','D'};
+	public static final char[] col4 = {'x','4','8','C','x'};
+	public static final char[] col5 = {'x','x','9','x','x'};
+	public static final char[][] keypadNums2 = {col1,col2,col3,col4,col5};
 	
 	public static void main (String[] args) throws IOException {
-		
-		int[][] keypadNums = {{1,4,7},{2,5,8},{3,6,9}};
 		
 		// Read through input file
 		try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
 			String line;
-			System.out.print("First code is : ");
+			String code1 = "";
+			String code2 = "";
 			while ((line = br.readLine()) != null) {
 				char[] steps = line.toCharArray();
+				
+				// iterate through each step for each part of the question
 				for (char c : steps) {
-					processMovement(c);
+					processMovement(c, 1);
+					processMovement(c, 2);
 				}
-				System.out.print(keypadNums[x][y]);
+				
+				// append the new step to the code
+				code1 = code1 + keypadNums[x1][y1];
+				code2 = code2 + keypadNums2[x2][y2];
 			}
-			System.out.println();
+			// print answers
+			System.out.println("answer to part 1 is : " + code1 + "\n" + "answer to part 2 is : " + code2);
 		}
 	}
-	private static void processMovement(char c) {
-		switch (c) {
-		case 'R' :
-			moveRight();
-			break;
-		case 'L' :
-			moveLeft();
-			break;
-		case 'U' :
-			moveUp();
-			break;
-		case 'D' :
-			moveDown();
-			break;
-				
-	}
-	}
-	
-	private static void moveUp() {
-		if (y == 0) return;
-		y--;
-	}
-	private static void moveDown() {
-		if (y == 2) return;
-		y++;
-	}
-	private static void moveLeft() {
-		if (x == 0) return;
-		x--;
-	}
-	private static void moveRight() {
-		if (x == 2) return;
-		x++;
+	private static void processMovement(char step, int part) {
+
+		char[][] pad;
+		int x, y;
+		
+		if (part == 1) {
+			pad = keypadNums;
+			x = x1;
+			y = y1;
+		} else {
+			pad = keypadNums2;
+			x = x2;
+			y = y2;
+		}
+		
+		// check the step to be taken and if it is possible
+		if (step == 'U' && y != 0 && pad[x][y-1] != 'x') {
+			if (part == 1) y1--;
+			if (part == 2) y2--;
+		}
+		if (step == 'D' && y+1 != pad.length && pad[x][y+1] != 'x') {
+			if (part == 1) y1++;
+			if (part == 2) y2++;
+		}
+		if (step == 'L' && x != 0 && pad[x-1][y] != 'x') {
+			if (part == 1) x1--;
+			if (part == 2) x2--;
+		}
+		if (step == 'R' && x+1 != pad.length && pad[x+1][y] != 'x') {
+			if (part == 1) x1++;
+			if (part == 2) x2++;
+		} 
+		
 	}
 }
