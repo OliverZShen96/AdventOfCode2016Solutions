7c9bd8474d60ed429bb7a2340149d5acce760e3e
OliverZShen96 <oliver.z.shen96@gmail.com>
2016-12-08 14:52:40 +1100

    Finished day 4

--- src/Day4.java
+++ src/Day4.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -10,6 +12,11 @@ public class Day4 {
 	private static BufferedReader br;
 
 	public static void main(String[] args) throws IOException {
+		part1();
+		part2();
+	}
+
+	private static void part1() throws NumberFormatException, IOException {
 		br = new BufferedReader(new FileReader("input.txt"));
 		String line = "";
 		int count = 0;
@@ -42,5 +49,29 @@ public class Day4 {
 		    if (correctChecksum.equals(checkSum)) count += sectionID;
 		}
 		System.out.println("Answer to part 1 is " + count);
+	}
+	
+	private static void part2() throws NumberFormatException, IOException {
+		br = new BufferedReader(new FileReader("input.txt"));
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			String[] parts = line.split("-|\\[|\\]");
+			int sectionID = Integer.parseInt(parts[parts.length-2]);
+			String checkSum = parts[parts.length-1];
+			int change = sectionID%26;
+			
+			for (int i = 0; i < parts.length-2; i++) {
+				for (char c : parts[i].toCharArray()) {
+					for (int j = change; j > 0; j--) {
+						if (c == 'z') c = 'a';
+						else c++;
+					}
+					System.out.print(c);
+				}
+				System.out.print(" ");
+			}
+			System.out.println(sectionID);
+
+		}
 	}
 }
