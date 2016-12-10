64b591a5e60e5426899e764acd1a5226b7052d3b
OliverZShen96 <oliver.z.shen96@gmail.com>
2016-12-10 18:32:54 +1100

    Completed day 7 part 1

---
+++ src/Day7.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day7 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		String line = "";
+		int count = 0;
+		while ((line = br.readLine()) != null) {
+			boolean hypernetABBA = false;
+			boolean normalABBA = false;
+			
+			Pattern p = Pattern.compile("\\[[^\\[]*\\]");
+			Matcher m = p.matcher(line);
+			while (m.find()) {
+				String match = m.group();
+				if (match.matches(".*(.)(?!\\1)(.)\\2\\1.*")) {
+					hypernetABBA = true;
+				}
+			}
+			String textOutsideBrackets = line.replaceAll("\\[[^\\[]*\\]", "XOX");
+			if (textOutsideBrackets.matches(".*(.)(?!\\1)(.)\\2\\1.*")) normalABBA = true;
+			
+			if (!hypernetABBA && normalABBA) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
