94db0042111ecc577e9f43d9934173bd380f5cd1
OliverZShen96 <oliver.z.shen96@gmail.com>
2016-12-14 16:17:15 +1100

    Finished day 7

--- src/Day7.pl
+++ src/Day7.pl
@@ -1,23 +1,35 @@
 #!/usr/bin/perl 
 $count = 0;
 while ($line = <>) {
-	$SSL = 0;
 	chomp $line;
 	my @ABAS;
+
+	# The various bracket expressions
 	my @inBrackets = ($line =~ /\[\w*\]/g);
+
+	# The rest of the line
 	$line =~ s/\[\w*\]//g;
+
+	# Find all ABAS present within square brackets and store them inside an array
 	foreach $bracketExpression (@inBrackets){
-		push @ABAS, $1 if ($bracketExpression =~ /((\w)(?!\2)(\w)\2)/g);
+		push @ABAS, ($bracketExpression =~ /(?=((.)(?!\2).\2))/g);
 	}
 
+	# Couldn't figure out how to only store actual matches
+	# Backref matches are also stored, so they are deleted afterwards
+	foreach my $index (0 .. $#ABAS) {
+        delete $ABAS[$index] if (length($ABAS[$index]) != 3);
+    }
+
+    # Search for occurences of corresponding BAB for each ABA
 	foreach $ABA (@ABAS) {
 		my @chars = split("", $ABA);
 		$BAB = @chars[1].@chars[0].@chars[1];
 		if ($line =~ /$BAB/) {
-			$SSL = 1; 
+			$count++;
+			last;
 		}
 	}
-	$count++ if ($SSL);
 
 }
 print "$count\n";
